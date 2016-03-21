ae54b27e9080692a827aee4355ae69a90bd5d67f
Nateowami <nateowami@gmail.com>
2016-03-21 12:56:59 +0800

    Add database connection for storing signatures

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Code for website live at [DearKasich.com](https://dearkasich.com)
 
+# TODO
+
+* Asthetics. It's ugly. Minimalist, clean look is what we're after.
--- index.js
+++ index.js
@@ -2,30 +2,35 @@ var express = require('express');
 var app = express();
 var cons = require('consolidate');
 var bodyParser = require('body-parser')
+var db = require('monk')('localhost/dearkasich');
+var signers = db.get('signers');
 
 //settings
 app.engine('html', cons.handlebars);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.use('/static', express.static('public'));
+app.use(require('morgan')('combined'));
 
 //data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 //data
-var signatures = [
-  { name: "Bob Joe", state: "JK"}
-]
+var cache = [];
+
+signers.find({}, {sort: {date: 1}}, function (e, signers) {
+  if(e) console.log(e);
+  cache = signers || [];
+});
 
 //logging
 app.use(function(req, res, next){
-  console.log("Request for " + req.url);
   next();
 });
 
 app.get('/', function (req, res) {
-  res.render('index', {signatures: signatures});
+  res.render('index', {signatures: cache});
 });
 
 app.post('/sign', function(req, res){
@@ -35,18 +40,20 @@ app.post('/sign', function(req, res){
     state: b.state,
     publicly: b.publicly,
     email: b.email,
-    meta: b.meta //meta-data, for helping make sure users are unique, we log IP, useragent, etc., not unlike most analytics solutions
+    meta: b.meta, //meta-data, for helping make sure users are unique, we log IP, useragent, etc., not unlike most analytics solutions
+    date: new Date()
   }
   
-  console.log(data);
+  cache.push(data);
   
-  signatures.push(data);
-  
-  res.send(JSON.stringify({
-    name: data.name,
-    state: data.state,
-    publicly: data.publicly
-  }));
+  signers.insert(data, function(error){
+    console.log(error);
+    res.send(JSON.stringify({
+      name: data.name,
+      state: data.state,
+      publicly: data.publicly
+    }));
+  });
   
 });
 
